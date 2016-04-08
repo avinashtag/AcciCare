350723a339566dc5f13f734d35bd041d0d85eb45
Avinash Tag <avi.tag@gmail.com>
2016-04-08 22:42:48 +0530

    Initial Commit

---
+++ AcciCare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AcciCare
+//
+//  Created by Avinash Tag on 08/04/16.
+//  Copyright © 2016 Avinash Tag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AcciCare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AcciCare
+//
+//  Created by Avinash Tag on 08/04/16.
+//  Copyright © 2016 Avinash Tag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
